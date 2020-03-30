27f4d80c6cc1f3a8f39c482d0a421369115e078b
himynameisdave <dave@fossa.com>
2020-03-30 16:33:11 -0700

    ✨ Init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## svelte-flex
+> _💪 A reusable flexbox component for Svelte_
+
+Work in progress!
---
+++ src/Flex.svelte
@@ -0,0 +1,46 @@
+<script>
+
+  export let direction = 'row';
+  export let align = 'center';
+  export let justify = 'center';
+
+  let className = `flex flex-direction--${direction} flex-align--${align} flex-justify--${justify}`;
+
+</script>
+
+<style>
+  .flex {
+    display: flex;
+  }
+  /* Direction: row | column */
+  .flex-direction--row {
+    flex-direction: row;
+  }
+  .flex-direction--column {
+    flex-direction: column;
+  }
+  /* Align: 'start' | 'center' | 'end' */
+  .flex-align--start {
+    align-items: flex-start;
+  }
+  .flex-align--center {
+    align-items: center;
+  }
+  .flex-align--end {
+    align-items: flex-end;
+  }
+  /* Justfy: 'start' | 'center' | 'end' */
+  .flex-justify--start {
+    justify-content: flex-start;
+  }
+  .flex-justify--center {
+    justify-content: center;
+  }
+  .flex-justify--end {
+    justify-content: flex-end;
+  }
+</style>
+
+<div class={className}>
+	<slot></slot>
+</div>
